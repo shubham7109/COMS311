f3dbf7d590b6b50b1df613812da4238db04160f0
Charli3theDoggo <30900396+Charli3theDoggo@users.noreply.github.com>
2019-03-22 15:23:09 -0500

    Update Test.java
    Created a simple test case.

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,11 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        Intervals test = new Intervals();
+        test.intervalInstert(2,3);
+        test.intervalInstert(1,3);
+        test.intervalInstert(4,7);
+        test.printRBTree(test.getRBTree().root);
     }
 
 }
