0893841571c64956ae8a90f4650f99bdfca658bc
Charli3theDoggo <30900396+Charli3theDoggo@users.noreply.github.com>
2019-03-22 15:29:41 -0500

    Fixed tests to reflect name fix of intervalInsert

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,9 @@ public class Test {
 
     public static void main(String[] args) {
         Intervals test = new Intervals();
-        test.intervalInstert(2,3);
-        test.intervalInstert(1,3);
-        test.intervalInstert(4,7);
+        test.intervalInsert(2,3);
+        test.intervalInsert(1,3);
+        test.intervalInsert(4,7);
         test.printRBTree(test.getRBTree().root);
     }
 
