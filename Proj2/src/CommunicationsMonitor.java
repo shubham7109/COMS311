c41560cc6fc469af242f99a209a17fd6644243b4
shubham <shubham@iastate.edu>
2019-04-11 15:21:33 -0500

    Init proj 2

---
+++ Proj2/src/CommunicationsMonitor.java
@@ -0,0 +1,14 @@
+public class CommunicationsMonitor {
+
+    public CommunicationsMonitor(){
+
+    }
+
+    //Should run in O(1)
+    public void addCommunication(int c1, int c2, int timestamp){
+
+    }
+
+    
+
+}
